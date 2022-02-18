e99a43c78a0af631212e893f2d0cbae1cd3175cb
Tatyana Asenova <tatyana.asenova@mentormate.com>
2022-02-18 18:06:41 +0200

    Using state and useEffect to create navbar functionality - open, close and resize

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classes from './Header.module.scss';
 
 import {BiMenuAltRight} from 'react-icons/bi';
 import {AiOutlineClose} from 'react-icons/ai';
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(true)
+    const [size, setSize] = useState({
+        width:undefined,
+        height:undefined
+    })
+
+    useEffect(() => {
+        const handleResize = () => {
+            setSize({
+                width: window.innerWidth,
+                height: window.innerHeight
+            })
+        }
+        window.addEventListener('resize', handleResize)
+
+        return () => window.removeEventListener('resize', handleResize)
+    }, [])
+
+    useEffect(() => {
+        if (size.width > 768 && menuOpen) {
+            setMenuOpen(false)
+        }
+    }, [size.width, menuOpen])
+    
+    const menuToggleHandler = () => {
+        setMenuOpen(state => !state)
+    }
   return (
     <header className={classes.header}>
         <div className={classes.header__content}>
             <h2 className={classes.header__content__logo}>Navbar</h2>
        
-        <nav className={classes.header__content__nav}>
+        <nav className={`${classes.header__content__nav} ${menuOpen ? classes.isMenu : ""}`}>
             <ul>
                 <li>
                     <a href="/">Page1</a>
@@ -25,7 +52,7 @@ const Header = () => {
             <button>CTA Page</button>
         </nav>
         <div className={classes.header__content__toggle}>
-            <BiMenuAltRight />
+           { !menuOpen ? <BiMenuAltRight onClick={menuToggleHandler}/> : <AiOutlineClose onClick={menuToggleHandler} />}
         </div>
         </div>
     </header>
